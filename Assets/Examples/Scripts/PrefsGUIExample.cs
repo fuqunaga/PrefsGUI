af0a60f625d5f52eaf1db906215399542060d20b
fuqunaga <fukhid@gmail.com>
2021-11-25 19:02:35 +0900

    tiny

--- Assets/Examples/Scripts/PrefsGUIExample.cs
+++ Assets/Examples/Scripts/PrefsGUIExample.cs
@@ -10,8 +10,10 @@ namespace PrefsGUI.Example
 
         private void Start()
         {
-            windows = new WindowLaunchers();
-            windows.isWindow = false;
+            windows = new WindowLaunchers
+            {
+                isWindow = false
+            };
             windows.Add("Part1", typeof(PrefsGUIExample_Part1));
             windows.Add("Part2", typeof(PrefsGUIExample_Part2));
             windows.Add("Part3", typeof(PrefsGUIExample_Part3));
