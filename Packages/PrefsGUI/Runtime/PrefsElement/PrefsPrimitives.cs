015452298af397d1f080f64e366984c78adcead9
fuqunaga <fukhid@gmail.com>
2023-05-08 19:02:00 +0900

    PrefsGUI.cs > PrefsGlobal.cs, PrefsPrimitives.cs

--- Packages/PrefsGUI/Runtime/PrefsElement/PrefsPrimitives.cs
+++ Packages/PrefsGUI/Runtime/PrefsElement/PrefsPrimitives.cs
@@ -7,36 +7,6 @@ using UnityEngine;
 
 namespace PrefsGUI
 {
-    #region Static
-    
-    public static class Prefs
-    {
-        public static void Save() => PrefsKvs.Save();
-        
-        public static void Load()
-        {
-            ClearCache();
-            PrefsKvs.Load();
-        } 
-        
-        public static void DeleteAll()
-        {
-            ClearCache();
-            PrefsKvs.DeleteAll();
-        }
-
-        private static void ClearCache()
-        {
-            foreach (var prefs in PrefsParam.all)
-            {
-                prefs.ClearCache();
-            }
-        }
-    }
-    
-    #endregion
-
-
     [Serializable]
     public class PrefsString : PrefsParamOuterInner<string, string>
     {
