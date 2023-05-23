ab134ae470318d078100b8d80049d057793d8e70
fuqunaga <fukhid@gmail.com>
2023-05-23 17:58:27 +0900

    refactor: PrefsList code new lines

--- Packages/PrefsGUI/Runtime/PrefsElement/PrefsList.cs
+++ Packages/PrefsGUI/Runtime/PrefsElement/PrefsList.cs
@@ -10,10 +10,12 @@ namespace PrefsGUI
     [Serializable]
     public class PrefsList<T> : PrefsAny<List<T>>, IList<T>, IList
     {
-        public PrefsList(string key, List<T> defaultValue = default) : base(key, defaultValue) { }
+        public PrefsList(string key, List<T> defaultValue = default) : base(key, defaultValue)
+        {
+        }
 
         public int DefaultValueCount => defaultValue?.Count ?? 0;
-        
+
         public bool IsDefaultCount => DefaultValueCount == Count;
 
         public void ResetToDefaultCount()
@@ -31,11 +33,11 @@ namespace PrefsGUI
                 {
                     list.RemoveRange(DefaultValueCount, listCount - DefaultValueCount);
                 }
-                
+
                 Set(list);
             }
         }
-        
+
         public bool IsDefaultAt(int idx)
         {
             if (idx < DefaultValueCount)
@@ -56,21 +58,34 @@ namespace PrefsGUI
                 Set(list);
             }
         }
- 
-        
+
+
         #region IList<T>
 
-        protected void UpdateValue(Action<List<T>> action) { var v = Get(); action(v); Set(v); }
+        protected void UpdateValue(Action<List<T>> action)
+        {
+            var v = Get();
+            action(v);
+            Set(v);
+        }
+
         public int Count => Get()?.Count ?? 0;
         public bool IsReadOnly => false;
-        public T this[int index] { get => Get()[index]; set { UpdateValue((v) => v[index] = value); } }
+
+        public T this[int index]
+        {
+            get => Get()[index];
+            set { UpdateValue((v) => v[index] = value); }
+        }
+
         public int IndexOf(T item) => Get().IndexOf(item);
-        public void Insert(int index, T item) => UpdateValue((v) => v.Insert(index, item));        
+        public void Insert(int index, T item) => UpdateValue((v) => v.Insert(index, item));
         public void RemoveAt(int index) => UpdateValue((v) => v.RemoveAt(index));
         public void Add(T item) => UpdateValue((v) => v.Add(item));
         public void Clear() => UpdateValue((v) => v.Clear());
         public bool Contains(T item) => Get().Contains(item);
         public void CopyTo(T[] array, int arrayIndex) => Get().CopyTo(array, arrayIndex);
+
         public bool Remove(T item)
         {
             var v = Get();
@@ -78,19 +93,22 @@ namespace PrefsGUI
             Set(v);
             return ret;
         }
+
         public IEnumerator<T> GetEnumerator() => Get().GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => Get().GetEnumerator();
 
         #endregion
-        
-        
+
+
         #region List like
+
         public void RemoveAll(Predicate<T> predicate) => UpdateValue(v => v.RemoveAll(predicate));
 
         #endregion
-        
-        
+
+
         #region IList
+
         public void CopyTo(Array array, int index)
         {
             throw new NotImplementedException();
@@ -105,8 +123,8 @@ namespace PrefsGUI
             set => this[index] = (T)value;
         }
 
-        public void Remove(object value) => Remove((T) value);
-        
+        public void Remove(object value) => Remove((T)value);
+
         public bool IsFixedSize => false;
 
         public int Add(object value)
@@ -114,11 +132,11 @@ namespace PrefsGUI
             throw new NotImplementedException();
         }
 
-        public bool Contains(object value) => Contains((T) value);
+        public bool Contains(object value) => Contains((T)value);
 
-        public int IndexOf(object value) => IndexOf((T) value);
+        public int IndexOf(object value) => IndexOf((T)value);
 
-        public void Insert(int index, object value) => Insert(index, (T) value);
+        public void Insert(int index, object value) => Insert(index, (T)value);
 
         #endregion
     }
